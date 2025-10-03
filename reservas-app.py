f30d23d7e1d8ecf293f3d4484fa7f04364a9613a
Denisse Zuniga <165586561+denisseza07@users.noreply.github.com>
2025-10-02 19:21:49 -0600

    Create reservas-app.py

---
+++ reservas-app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+import os
+from datetime import datetime
+
+
+app = Flask(__name__)
+#DATABASE = 'database.db'
+
+DATABASE = os.environ.get('DB_PATH', 'data/database.db')
+LOGFILE_PATH = os.environ.get('LOGFILE_PATH', 'data/registro.log')
+APP_ENV = os.environ.get('APP_ENV', 'development')
+
+def init_db():
+
+    if os.path.exists(DATABASE):
+        os.remove(DATABASE)
+        print("Base de datos anterior eliminada.")
+
+        with sqlite3.connect(DATABASE) as conn:
+            conn.execute('''
+                CREATE TABLE registros (
+                    rtc TEXT NOT NULL,
+                    nombre TEXT NOT NULL,
+                    fechaini DATE NOT NULL,
+                    fechafin DATE NOT NULL,
+                    scrum TEXT NOT NULL,
+                    estado TEXT NOT NULL
+                );
+            ''')
+            print("Base de datos creada.")
+
+
+#    if not os.path.exists(DATABASE):
+#        with sqlite3.connect(DATABASE) as conn:
+#            conn.execute('''
+#                CREATE TABLE registros (
+#                    rtc TEXT NOT NULL,
+#                    nombre TEXT NOT NULL,
+#                    fechaini DATE NOT NULL,
+#                    fechafin DATE NOT NULL,
+#                    scrum TEXT NOT NULL,
+#                    estado TEXT NOT NULL
+#                );
+#            ''')
+#            print("Base de datos creada.")
+
+
+@app.route('/')
+def index():
+    conn = sqlite3.connect(DATABASE)
+    registros = conn.execute("SELECT * FROM registros").fetchall()
+    conn.close()
+    return render_template('index.html', registros=registros)
+
+@app.route('/agregar', methods=['POST'])
+def agregar():
+    rtc = request.form['rtc']
+    nombre = request.form['nombre']
+    fechaini = request.form['fechaini']
+    fechafin = request.form['fechafin']
+    scrum = request.form['scrum']
+    estado = request.form['estado']
+
+
+    conn = sqlite3.connect(DATABASE)
+    conn.execute("INSERT INTO registros (rtc, nombre, fechaini, fechafin, scrum) VALUES (?, ?, ?, ?, ?, ?)", (rtc, nombre, fechaini, fechafin, scrum, estado))
+    conn.commit()
+    conn.close()
+
+
+    with open(LOGFILE_PATH, 'a') as f:
+        f.write(f"[{datetime.now()}] Nuevo registro: {rtc} - {nombre} - {fechaini} - {fechafin} - {scrum} - {estado}\n")
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    print(f"Entorno de aplicación: {APP_ENV}")
+    init_db()
+    app.run(debug=True, host='0.0.0.0', port=5000)
